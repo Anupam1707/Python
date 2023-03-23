5f6f165423e7f1f5071ca99713c021290367f112
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-03-23 21:33:41 +0530

    Add files via upload

---
+++ Functions/3. mulist.py
@@ -0,0 +1,27 @@
+#To multiply the elements of a listusing a function
+#mullist.py
+#23-3-23
+#By Anupam Kanoongo
+
+print("The List Sum Finder")
+print()
+
+def mullst(lst):
+    mul = 1
+    for i in lst:
+        mul *= i
+    return mul
+
+ls = eval(input("Enter a List : "))
+
+r = mullst(ls)
+
+print()
+print("The Sum of all elements of the above list is",r)
+
+##Output
+##The List Sum Finder
+##
+##Enter a List : [1,2,3,4,5,6]
+##
+##The Sum of all elements of the above list is 720
