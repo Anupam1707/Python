fd60531803d22cfdb845af60e7f26b5b874a804e
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-03-24 18:07:18 +0530

    Add files via upload

---
+++ Functions/13. pascaltri.py
@@ -0,0 +1,26 @@
+#To print the pascal's triangle with n rows
+#pascaltri.py
+#24-3-23
+#By Anupam Kanoongo
+
+print("Welcome to the Pascal's Triangle Printer")
+print()
+
+def pascal(n):
+    row = [1]
+    for i in range(n):
+        print(*row)
+        row = [1] + [row[j] + row[j+1] for j in range(i)] + [1]
+
+n = int(input("Enter Number of Rows : "))
+pascal(n)
+
+##Output
+##Welcome to the Pascal's Triangle Printer
+##
+##Enter Number of Rows : 5
+##1
+##1 1
+##1 2 1
+##1 3 3 1
+##1 4 6 4 1
