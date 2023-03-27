41564ad150b0a0442c49566ae3ec848ef76be4d0
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-03-27 19:50:14 +0530

    Update PyFetch.py

--- Tools/PyFetch.py
+++ Tools/PyFetch.py
@@ -3,6 +3,7 @@ from github import Github
 
 url1 = "https://raw.githubusercontent.com/Anupam1707/Python_Programmes/main/"
 url2 = "https://raw.githubusercontent.com/Anupam1707/weather-app-py/main/"
+url3 = "https://raw.githubusercontent.com/Anupam1707/AI_Assistant/main/"
 
 def python(name):
     page = requests.get(url1 + name)
@@ -10,4 +11,8 @@ def python(name):
 
 def weather(name):
     page = requests.get(url2 + name)
+    return page.text
+
+def ai(name):
+    page = requests.get(url3 + name)
     return page.text
