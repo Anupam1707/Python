62beedf0af54b6d25444082ef7ba93ed7f3255e9
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-04-05 10:15:04 +0530

    Update PyFetch.py

--- Tools/PyFetch.py
+++ Tools/PyFetch.py
@@ -1,7 +1,7 @@
 import requests
 from github import Github
 
-url1 = "https://raw.githubusercontent.com/Anupam1707/Python_Programmes/main/"
+url1 = "https://raw.githubusercontent.com/Anupam1707/Python/main/"
 url2 = "https://raw.githubusercontent.com/Anupam1707/weather-app-py/main/"
 url3 = "https://raw.githubusercontent.com/Anupam1707/AI_Assistant/main/main.py"
 
