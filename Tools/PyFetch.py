cb1cd832796a0338a41ff9679c2a7d95615f124b
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-04-26 23:10:43 +0530

    Add files via upload

--- Tools/PyFetch.py
+++ Tools/PyFetch.py
@@ -1,8 +1,10 @@
 import requests
+from github import Github
 
-url1 = "https://raw.githubusercontent.com/Anupam1707/Python/main/"
+url1 = "https://raw.githubusercontent.com/Anupam1707/Python_Programmes/main/"
 url2 = "https://raw.githubusercontent.com/Anupam1707/weather-app-py/main/"
-url3 = "https://raw.githubusercontent.com/Anupam1707/ai/main/main.py"
+url3 = "https://raw.githubusercontent.com/Anupam1707/ai/main/"
+url4 = "https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/"
 
 def python(name):
     page = requests.get(url1 + name)
@@ -12,6 +14,10 @@ def weather(name):
     page = requests.get(url2 + name)
     return page.text
 
-def ai():
-    page = requests.get(url3)
+def ai(name):
+    page = requests.get(url3 + name)
+    return page.text
+
+def food(name):
+    page = requests.get(url4 + name)
     return page.text
