f006091e0ce98c09e29eea76e8c07758748adc06
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-04-28 23:13:25 +0530

    .

--- File Handling/contacts.py
+++ File Handling/contacts.py
@@ -3,5 +3,19 @@
 #28-4-23
 #By Anupam Kanoongo
 
-def input():
-    with open("contacts.txt", "")
+def inp():
+    with open("contacts.txt", "a") as c:
+        name = None
+        phone = None
+        eml = None
+        while name != "":
+            name = str(input("Enter the Name : "))
+            while name != "":
+                phone = int(input("Enter the phone Number : "))
+                eml = str(input("Enter the Email : "))
+                c.write(f"Name : {name}\nPhone : {phone}\nEmail : {eml}\n\n")
+
+def out():
+    with open("contacts.txt", "r") as c:
+        d = c.readlines()
+    
