66d22023431ef2468afcece23a57069945f786af
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-04-28 22:41:06 +0530

    Update keyboard.py

---
+++ File Handling/keyboard.py
@@ -0,0 +1,22 @@
+#To save all lower case characters in a file "lower.txt", upper case characters in a file "upper.txt" and other characters in "others.txt"
+#keyboard.py
+#28-4-23
+#By Anupam Kanoongo
+
+import sys
+
+def check(var):
+    inp = ""
+    while inp == " ":
+        inp = sys.stdin.readline()
+        if inp.islower():
+            with open("lower.txt", "a") as l:
+                l.write(inp)
+        elif inp.isupper():
+            with opne("upper.txt", "a") as u:
+                u.write(inp)
+        else:
+            with open("others.txt", "a") as o:
+                o.write(inp)
+
+check()
