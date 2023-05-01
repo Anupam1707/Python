cf9a10c535efcbfdd6decf2f9bf502fc904555ff
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-05-01 19:48:11 +0530

    Update crypter.py

--- Tools/crypter.py
+++ Tools/crypter.py
@@ -1,4 +1,4 @@
-def Key(string, key):
+def pwd(string, key):
     key = list(key) 
     if len(string) == len(key):
         return(key) 
@@ -8,23 +8,26 @@ def Key(string, key):
         return("" . join(key)) 
   
 def encrypt(string,password):
-    string = string.upper()
-    key = Key(string, password)
+    string = string.upper() + " "
+    key = pwd(string, password)
     encrypt_text = []
     for i in range(len(string)):
-        x = (ord(string[i]) +ord(key[i])) % 26
-        x += ord('a')
+        x = (ord(string[i]) +ord(key[i].upper())) % 26
+        x += ord('A')
         encrypt_text.append(chr(x))
     encrypt_text = ("" . join(encrypt_text)).upper()
     key = ("".join(key)).upper()
-    return encrypt_text, key
+    return encrypt_text + key
 
-def decrypt(encrypt_text, key):
+def decrypt(encrypt_text):
     orig_text = []
-    encrypt_text.upper()
+    encrypt_text = encrypt_text.upper()
+    b = int(len(encrypt_text)/2)
+    key = encrypt_text[b:]
+    encrypt_text = encrypt_text[:b-1]
     for i in range(len(encrypt_text)):
-        x = (ord(encrypt_text[i]) -ord(key[i]) + 26) % 26
-        x += ord('a')
+        x = (ord(encrypt_text[i]) -ord(key[i].upper()) + 26) % 26
+        x += ord('A')
         orig_text.append(chr(x))
     orig_text = ("" . join(orig_text)).upper()
     return orig_text
