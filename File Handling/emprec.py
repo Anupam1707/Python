693afd9942c8f3251eca687f63fb0339f3d5c42c
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-06-28 12:46:33 +0530

    Add files via upload

---
+++ File Handling/emprec.py
@@ -0,0 +1,43 @@
+import pickle
+try:
+    with open("EmpRec.bin", "rb") as file:
+        pass
+except :
+    with open("EmpRec.bin", "wb") as file
+        pass
+    
+def entry():
+    empno = int(input("Enter Employee Number : "))
+    name = str(input("Enter the name of Employee : "))
+    designation = str(input("Enter the Designation of the Employee : "))
+    salary = int(input("Enter the Salary of Employee : "))
+    inp = {"No":empno, "Name":name, "Designation":designation, "Salary":salary}
+    with open("EmpRec.bin","ab") as file:
+        pickle.dump(inp, file)
+
+def search():
+    eno = int(input("Enter the Employee Number : "))
+    try:
+        with open("EmpRec.bin","rb") as file:
+            data = pickle.load(file)
+            for rec in data:
+                if rec["No"] == eno:
+                    print(rec)
+    except:
+        pass
+def report():
+    
+def main():
+    print("1. Give an Entry\n2. Update an Entry\n3. Search an Entry\nGenerate Report")
+    choice = int(input("Enter an Option : "))
+    while True:
+        if choice == 1:
+            entry()
+        elif choice == 2:
+            update()
+        elif choice == 3:
+            search()
+        elif choice == 4:
+            report()
+        else:
+            break
