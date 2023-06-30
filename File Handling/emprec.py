7f7857e248cbba8d055c0dae8978f8c5bb029f49
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-06-30 11:32:24 +0530

    Update emprec.py

--- File Handling/emprec.py
+++ File Handling/emprec.py
@@ -1,10 +1,15 @@
 import pickle
+def menu():
+    print("1. Give an Entry\n2. Update an Entry\n3. Search an Entry\n4. Generate Report")
+    choice = int(input("Enter an Option : "))
+    return choice
+
 def start():
     try:
-        with open("EmpRec.bin", "rb") as file:
+        with open("EmpRec.dat", "rb") as file:
             pass
     except :
-        with open("EmpRec.bin", "wb") as file:
+        with open("EmpRec.dat", "wb") as file:
             pass
     
 def entry():
@@ -13,27 +18,31 @@ def entry():
     designation = str(input("Enter the Designation of the Employee : "))
     salary = int(input("Enter the Salary of Employee : "))
     inp = {"No":empno, "Name":name, "Designation":designation, "Salary":salary}
-    with open("EmpRec.bin","ab") as file:
+    with open("EmpRec.dat","ab") as file:
         pickle.dump(inp, file)
     print("Successfully Entered")
     print()
-
+    main()
+    
 def search():
     eno = int(input("Enter the Employee Number : "))
     try:
-        with open("EmpRec.bin","rb") as file:
-          file.seek(eno)
-          data = pickle.load(file)
+        with open("EmpRec.dat","rb") as file:
+          for i in range(eno):
+              data = pickle.load(file)
+          file.seek(0)
           for key in data.keys():
-              print(key, data[key])
+              print(key, "-",data[key])
           print()
+        main()
     except:
         print("No record found with the id", eno)
         print()
+        main()
 def report():
     sum = 0
     try:
-        with open("EmpRec.bin", "rb") as file:
+        with open("EmpRec.dat", "rb") as file:
             while True:
                 data = pickle.load(file)
                 sum += data["Salary"]
@@ -43,8 +52,7 @@ def report():
     
 def main():
     start()
-    print("1. Give an Entry\n2. Update an Entry\n3. Search an Entry\nGenerate Report")
-    choice = int(input("Enter an Option : "))
+    choice = menu()
     while True:
         if choice == 1:
             entry()
@@ -56,3 +64,4 @@ def main():
             report()
         else:
             break
+main()
